df87924f46ef5da0ec02e822b28e79d94c7874e8
Hernán López <her3814@hotmail.com>
2023-02-18 21:11:07 -0300

    Added some initial testing of Auth hashing and salting

--- webapi/EnigmaBudget/EnigmaBudget.Backend.Tests/Auth/AuthTests.cs
+++ webapi/EnigmaBudget/EnigmaBudget.Backend.Tests/Auth/AuthTests.cs
@@ -1,3 +1,5 @@
+using EnigmaBudget.Infrastructure.Helpers;
+using EnigmaBudget.Infrastructure.Auth.Helpers;
 namespace EnigmaBudget.Backend.Tests.Auth
 {
     public class Tests
@@ -8,9 +10,28 @@ namespace EnigmaBudget.Backend.Tests.Auth
         }
 
         [Test]
-        public void Test1()
+        public void PasswordHashDoesntChange()
         {
-            Assert.Pass();
+
+            string password = "Password1234";
+            string salt = HashHelper.CreateSalt();
+            var encryptedPass = HashHelper.HashPassword(password, salt);
+            var newEncryptedPass = HashHelper.HashPassword(password, salt);
+
+            Assert.That(newEncryptedPass, Is.EqualTo(encryptedPass));
+        }
+        [Test]
+        public void SaltsDifferBetweenCreations()
+        {
+
+            string password = "Password1234";
+            string saltA = HashHelper.CreateSalt();
+            string saltB = HashHelper.CreateSalt();
+            var encryptedPass = HashHelper.HashPassword(password, saltA);
+            var newEncryptedPass = HashHelper.HashPassword(password, saltB);
+
+            Assert.That(saltA, !Is.EqualTo(saltB));
+            Assert.That(encryptedPass, !Is.EqualTo(newEncryptedPass));
         }
     }
 }
